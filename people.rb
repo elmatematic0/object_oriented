fd82c4d7151e6853e0c62e263984359e864219f4
elmatematic0 <wkdalebout+github@gmail.com>
2013-10-24 15:27:54 -0400

    completing tasks through number 7.

--- people.rb
+++ people.rb
@@ -1,10 +1,24 @@
-class Student
+
+class Person
+  def initialize(name)
+    @name = name
+    puts "Hi, my name is #{name}!"
+  end
+end
+
+class Student<Person
   def learn
     puts "I get it!"
   end
 end
 
-class Instructor
+class Instructor<Person
   def teach
     puts "Everything in Ruby is an Object"
   end
+end
+
+chris = Instructor.new("Chris")
+puts chris
+cristina = Student.new("Cristina")
+puts cristina
